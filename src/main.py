87866935fb08f4c7a937a88177a22671a07a7cda
Mariana <villegasmariana398@gmail.com>
2026-01-29 14:43:36 -0500

    c5

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 @app.route("/")
 def index():
     return render_template("index.html")
